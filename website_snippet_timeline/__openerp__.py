f6fe88ffb2ead52d31318db984144e365bc46272
krocket <fabian.monoyer@gmail.com>
2015-02-07 13:09:09 +0100

    nouveau fichier: README.md 	nouveau fichier: __init__.py x#	nouveau fichier: __openerp__.py x#	nouveau fichier: static/description/icon.png x#	nouveau fichier: static/description/img1.png x#	nouveau fichier: static/description/index.html x#	nouveau fichier: static/img/blocks/block_title.png x#	nouveau fichier: static/style/snippet.css x#	nouveau fichier: views/options.xml x#	nouveau fichier: views/snippets.xml

---
+++ website_snippet_timeline/__openerp__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) Monoyer Fabian (fabian.monoyer@gmail.com)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+
+{
+    # Theme information
+    'name': "Snippet timeline ",
+    'description': """
+    """,
+    'category': 'Theme',
+    'version': '1.0',
+    'depends': ['website'],
+
+    # templates, pages, and snippets
+    'data': [
+        'views/options.xml',
+        'views/snippets.xml',
+   ],
+
+    # Your information
+    'application':True,
+    'author': "Odoo Labs",
+    'website': "",
+}
